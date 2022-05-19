b25dbc92f77400ecfb250d115850795cf1fd35d2
dankolar69 <91247767+dankolar69@users.noreply.github.com>
2022-05-19 09:16:09 +0200

    fix

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
 basic.show_icon(IconNames.HEART)
-def on_forever():
-    pass
-basic.forever(on_forever)
+
--- main.ts
+++ main.ts
@@ -1,4 +1,1 @@
 basic.showIcon(IconNames.Heart)
-basic.forever(function on_forever() {
-    
-})
